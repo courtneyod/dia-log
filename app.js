c1a47097025baa6c5ad03e0cbdc91fffe7c9b78a
Courtney ODonnell <courtney.od@gmail.com>
2017-02-13 12:01:52 -0800

    added ellies cors suggestion

--- app.js
+++ app.js
@@ -50,11 +50,30 @@ app.use('/signup', signup)
 app.use('/categories', categories)
 app.use('/health-stat-categories', health_stat_categories)
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+// ## CORS middleware
+// For more info see: https://gist.github.com/cuppster/2344435
+//
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
