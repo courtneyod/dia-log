db7a488121fe3c80fe26cb6d6f88287b59da3759
Courtney ODonnell <courtney.od@gmail.com>
2017-02-13 12:18:04 -0800

    moved cors

--- app.js
+++ app.js
@@ -13,6 +13,25 @@ const cors = require('cors')
 
 app.use(cors())
 
+// ## CORS middleware
+// For more info see: https://gist.github.com/cuppster/2344435
+//
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
 var aws = require('./routes/auth');
 var settings = require('./routes/settings');
 var photos = require('./routes/photos');
@@ -40,24 +59,7 @@ app.use(morgan('combined'))
 //     });
 //   }
 // ));
-// ## CORS middleware
-// For more info see: https://gist.github.com/cuppster/2344435
-//
-// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
 
 app.use('/aws', aws)
 app.use('/settings', settings)
